e99a41e09c02d2234e5558eb31b69947de8c7819
Ben Maaketad <bemaakestad@gmail.com>
2022-06-27 23:51:10 -0500

    Add resource scriptable object
    Add tree prefab with simple scriptable object

---
+++ Assets/Scripts/ResourcePlacer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Resource
+{
+    public Transform transform;
+    protected ResourceData _data;
+}
+
+public class ResourcePlacer : MonoBehaviour
+{
+    public static ResourcePlacer instance;
+
+    private void Start()
+    {
+        instance = this;
+
+    }
+
+    public void SpawnResource(Vector3 position)
+    {
+
+    }
+}
